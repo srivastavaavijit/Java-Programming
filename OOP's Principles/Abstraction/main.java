7d068111cdeaeb324cc6cbd48f6347aca36fb011
Avijit Srivastava <163127742+srivastavaavijit@users.noreply.github.com>
2024-10-05 19:35:08 +0530

    main.java

---
+++ OOP's Principles/Abstraction/main.java
@@ -0,0 +1,21 @@
+package introduction.OOP.Abstraction;
+
+public class main {
+    public static void main(String[] args) {
+
+//        ATM person1 = new ATM(10000);
+//
+//        person1.checkBalance();
+//
+//        person1.deposit(20000);
+//
+//        person1.checkBalance();
+//
+//        person1.withdraw(5000);
+//
+//        person1.checkBalance();
+//
+        ATM person2 = new ATM(5000);
+        person2.start();
+    }
+}
