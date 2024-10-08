08d20bd4f4fc612fcc06267faf3d15c99fa82ddb
Avijit Srivastava <163127742+srivastavaavijit@users.noreply.github.com>
2024-10-08 08:48:09 +0530

    Box.java

---
+++ OOP's Principles/Inheritance/Box.java
@@ -0,0 +1,44 @@
+package introduction.OOP.Inheritance;
+
+public class Box {
+    // Accessible only in this file.
+    private double l;
+    double h;
+    double w;
+
+    Box(){
+        // all classes are inherited from object class.
+        super();
+        this.l = -1;
+        this.h = -1;
+        this.w = -1;
+    }
+
+    // cube:
+    Box(double side){
+        this.w = side;
+        this.l = side;
+        this.h = side;
+    }
+
+    Box(double l, double h, double w){
+        this.l = l;
+        this.h = h;
+        this.w = w;
+    }
+
+    Box(Box old)
+    {
+        this.h = old.h;
+        this.l = old.l;
+        this.w = old.w;
+    }
+
+    public void information(){
+        System.out.println("Running the box.");
+    }
+
+    static void greeting(){
+        System.out.println("Hello, Welcome to Box class!");
+    }
+}
