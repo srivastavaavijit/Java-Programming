289c3336d654c5242b71dd351da536aec5135fb3
Avijit Srivastava <163127742+srivastavaavijit@users.noreply.github.com>
2024-10-09 22:35:38 +0530

    Dog.java

---
+++ OOP's Principles/Interface/Dog.java
@@ -0,0 +1,14 @@
+package introduction.OOP.Interface;
+
+public class Dog implements Animal{
+    @Override
+    public void eat() {
+        System.out.println("Dog is eating.");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Dog is sleeping.");
+    }
+
+}
