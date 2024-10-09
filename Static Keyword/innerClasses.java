39881e32c1f1a288cc25953e6ad4f1795b522226
Avijit Srivastava <163127742+srivastavaavijit@users.noreply.github.com>
2024-10-09 08:25:16 +0530

    Create inner classes.java

---
+++ Static Keyword/innerClasses.java
@@ -0,0 +1,20 @@
+package introduction.OOP.staticKeyword;
+
+//outside classes cannot be static, only inner classes can be static.
+public class innerClasses {
+    static class Test{
+        String name;
+
+        public Test(String name){
+            this.name = name;
+        }
+    }
+
+    public static void main(String[] args) {
+        Test a = new Test("Kunal");
+        Test b = new Test("Rahul");
+
+        System.out.println(a.name);
+        System.out.println(b.name);
+    }
+}
