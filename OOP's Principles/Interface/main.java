17e97bb4bce446714aea424b0bd672a69a6956ec
Avijit Srivastava <163127742+srivastavaavijit@users.noreply.github.com>
2024-10-09 22:34:49 +0530

    Create main.java

---
+++ OOP's Principles/Interface/main.java
@@ -0,0 +1,18 @@
+package introduction.OOP.Interface;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+
+        d.eat();
+
+        d.sleep();
+
+        d.makeSound();
+
+        Animal.info();
+
+
+    }
+}
